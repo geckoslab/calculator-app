e53571f0bdca35a2290bd6d563b485ff2007ba97
Dat Tran <tyler.tran.it@gmail.com>
2020-12-15 17:23:34 +0700

    improve code conventions

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,44 @@
 import { app, BrowserWindow } from 'electron';
 
+/**
+ * Render Electron application.
+ */
 const createWindow = async () => {
   let window = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
+      contextIsolation: true,
     },
     autoHideMenuBar: true,
-    frame: false,
   });
 
   await window.loadFile('src/ui/index.html');
 };
 
-app.whenReady().then(createWindow)
+/**
+ * Application entry point.
+ * 1. TODO: Connect to NLP service...
+ * 2. Render application.
+ */
+app.whenReady()
+  .then(() => {
+    console.log('TODO: Connect to NLP service...');
+  })
+  .then(createWindow);
 
+/**
+ * Application window events.
+ */
+// On close all application windows.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
+// On start application event.
 app.on('activate', async () => {
   await createWindow();
 });
